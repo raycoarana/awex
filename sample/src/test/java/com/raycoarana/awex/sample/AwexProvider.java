f55ae26816567aac81f03b3a5b3234815f2c1578
raycoarana <me@raycoarana.com>
2016-02-06 21:47:27 +0100

    Fix compilation error in AwexProvider

--- sample/src/test/java/com/raycoarana/awex/sample/AwexProvider.java
+++ sample/src/test/java/com/raycoarana/awex/sample/AwexProvider.java
@@ -3,17 +3,17 @@ package com.raycoarana.awex.sample;
 import com.raycoarana.awex.Awex;
 import com.raycoarana.awex.android.AndroidLogger;
 import com.raycoarana.awex.android.AndroidUIThread;
+import com.raycoarana.awex.policy.LinearWithRealTimePriority;
 
 public class AwexProvider {
 
-    private static final int MIN_THREADS = 2;
     private static final int MAX_THREADS = 4;
 
     private static Awex sInstance = null;
 
     public static synchronized Awex get() {
         if(sInstance == null) {
-            sInstance = new Awex(new AndroidUIThread(), new AndroidLogger(), MIN_THREADS, MAX_THREADS);
+            sInstance = new Awex(new AndroidUIThread(), new AndroidLogger(), new LinearWithRealTimePriority(MAX_THREADS));
         }
         return sInstance;
     }
